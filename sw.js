bc174660ccd2481bd50f54012f3a92be50ca46dc
yakovbronnikov <jaguation@gmail.com>
2022-01-14 20:04:46 +1000

    SW fix

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const cacheName = 'funmath-v1';
+const timeout = 400;
 // При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
     event.waitUntil(
@@ -16,25 +17,31 @@ self.addEventListener('install', (event) => {
         ));
 });
 
-// при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
-self.addEventListener('fetch', function(event) {
-    // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
-    event.respondWith(fromCache(event.request));
-    // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
-    event.waitUntil(update(event.request));
+// при событии fetch, мы и делаем запрос, но используем кэш, только после истечения timeout.
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fromNetwork(event.request, timeout)
+      .catch((err) => {
+          console.log(`Error: ${err.message()}`);
+          return fromCache(event.request);
+      }));
 });
 
+// Временно-ограниченный запрос.
+function fromNetwork(request, timeout) {
+    return new Promise((fulfill, reject) => {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then((response) => {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
+
 function fromCache(request) {
-    return caches.open(cacheName).then((cache) =>
+// Открываем наше хранилище кэша (CacheStorage API), выполняем поиск запрошенного ресурса.
+// Обратите внимание, что в случае отсутствия соответствия значения Promise выполнится успешно, но со значением `undefined`
+    return caches.open(CACHE).then((cache) =>
         cache.match(request).then((matching) =>
             matching || Promise.reject('no-match')
         ));
-}
-
-function update(request) {
-    return caches.open(cacheName).then((cache) =>
-        fetch(request).then((response) =>
-            cache.put(request, response)
-        )
-    );
 }
