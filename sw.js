24e9531a9fd13fbf1bed3cc226838b7227cd115b
yakovbronnikov <jaguation@gmail.com>
2022-01-20 22:19:24 +1000

    Update SW

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const cacheName = 'funmath-v1';
-const timeout = 400;
-// При установке воркера мы должны закешировать часть данных (статику).
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(cacheName).then((cache) => cache.addAll([
@@ -23,30 +22,26 @@ self.addEventListener('install', (event) => {
         ));
 });
 
-// при событии fetch, мы и делаем запрос, но используем кэш, только после истечения timeout.
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fromNetwork(event.request, timeout)
-      .catch((err) => {
-          return fromCache(event.request);
-      }));
+
+// при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
+self.addEventListener('fetch', function(event) {
+    // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
+    event.respondWith(fromCache(event.request));
+    // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
+    event.waitUntil(update(event.request));
 });
 
-// Временно-ограниченный запрос.
-function fromNetwork(request, timeout) {
-    return new Promise((fulfill, reject) => {
-        var timeoutId = setTimeout(reject, timeout);
-        fetch(request).then((response) => {
-            clearTimeout(timeoutId);
-            fulfill(response);
-        }, reject);
-    });
-}
-
 function fromCache(request) {
-// Открываем наше хранилище кэша (CacheStorage API), выполняем поиск запрошенного ресурса.
-// Обратите внимание, что в случае отсутствия соответствия значения Promise выполнится успешно, но со значением `undefined`
     return caches.open(cacheName).then((cache) =>
         cache.match(request).then((matching) =>
             matching || Promise.reject('no-match')
         ));
+}
+
+function update(request) {
+    return caches.open(cacheName).then((cache) =>
+        fetch(request).then((response) =>
+            cache.put(request, response)
+        )
+    );
 }
