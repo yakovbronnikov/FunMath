a9858c65a13cdc6b184451ebdcb94f532dfbf82c
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2022-01-21 11:14:20 +1000

    SW fix

--- sw.js
+++ sw.js
@@ -28,7 +28,6 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(fromNetwork(event.request, timeout)
       .catch((err) => {
-          console.log(`Error: ${err.message()}`);
           return fromCache(event.request);
       }));
 });
